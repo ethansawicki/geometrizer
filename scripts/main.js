abb16719e5c24f689beb1771184fba5481d77157
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-08-02 00:54:52 -0500

    adding JS files from practice exercises

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const radius = 7
+const diameter = 2 * radius
+const circumference = 2 * 3.141592 * radius
+const area = 3.141592 * radius ** 2
+
+console.log(`The circumference is ${circumference}`)
+console.log(`The Area of the circle is ${area}`)
